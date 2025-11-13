dc520b169c7319205027604c20a989cfbd8a08a6
itsAradhyahere <vishwaradhyachavani@gmail.com>
2025-11-13 13:26:12 +0530

    Appscrip-task-Vishwaradhya

--- appscrip-task-vishwaradhya/src/utils/api.js
+++
@@ -1,9 +0,0 @@
-export async function getProducts() {
-  try {
-    const res = await fetch("https://fakestoreapi.com/products");
-    return await res.json();
-  } catch (error) {
-    console.log("API Error:", error);
-    return [];
-  }
-}
